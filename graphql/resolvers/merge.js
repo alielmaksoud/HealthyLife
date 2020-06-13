3a4ea04df8b042e9b4875d0989585c0a64c1c6c3
reyhanboydag <reyhanboydag@gmail.com>
2020-06-13 22:43:49 +0300

    using dataloader

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,41 +1,50 @@
+const DataLoader = require('dataloader');
+
 const Event = require('../../models/events');
 const User = require('../../models/user');
 
 const {dateToString} = require('../../helpers/date');
 
-const events = async eventIds=>{
-    try {
-        const events= await Event.find({_id:{$in:eventIds}
-    });
-    return events.map(event=>{
-        return transformEvent(event);
-    }); 
-    }catch(err){
-        throw err;
-    }
-  };
+const eventLoader = new DataLoader((eventIds) => {
+  return events(eventIds);
+});
 
-  const singleEvent=async eventId=>{
-    try{
-      const event =await Event.findById(eventId);
+const userLoader = new DataLoader(userIds => {
+  return User.find({_id: {$in: userIds}});
+});
+
+const events = async eventIds => {
+  try {
+    const events = await Event.find({ _id: { $in: eventIds } });
+    return events.map(event => {
       return transformEvent(event);
-    }catch (err){
-      throw err
-    }
+    });
+  } catch (err) {
+    throw err;
   }
-  
-  const user= async userId=>{
-      try{
-    const user = await User.findById(userId)
-      return {
-        ...user._doc,
-        _id:user.id,
-        createdEvents:events.bind(this,user._doc.createdEvents)
-      };
-    }catch(err){
-        throw err
+};
+
+const singleEvent = async eventId => {
+  try {
+    const event = await eventLoader.load(eventId.toString());
+    return event;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const user = async userId => {
+  try {
+    const user = await userLoader.load(userId.toString());
+    return {
+      ...user._doc,
+      _id: user.id,
+      createdEvents: eventLoader.load.bind(this, user._doc.createdEvents)
     };
+  } catch (err) {
+    throw err;
   }
+};
 
   const transformEvent = event =>{
     return {
