f3081c18c3294f31e528608dd679dc6f5056328f
reyhanboydag <reyhanboydag@gmail.com>
2020-07-01 11:29:51 +0300

    homepage edit

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -31,12 +31,14 @@ class HomePage extends Component {
   <Card className="p-3">
     <blockquote className="blockquote mb-0 card-body">
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
+      Vücudumuzun dengesini, ihtiyaçlarını karşılamak için  
+tek başına su yeterli değildir. 
+Bir diğer ihtiyacımız ise kaloridir.
+
       </p>
       <footer className="blockquote-footer">
         <small className="text-muted">
-          Someone famous in <cite title="Source Title">Source Title</cite>
+          
         </small>
       </footer>
     </blockquote>
@@ -59,12 +61,11 @@ class HomePage extends Component {
   <Card bg="primary" text="white" className="text-center p-3">
     <blockquote className="blockquote mb-0 card-body">
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
+Unutmayın: Kalori hesaplamaları kişiye özel olup cinsiyet, boy, kilo gibi bilgilere göre değişiklik göstermektedir.
+
       </p>
       <footer className="blockquote-footer">
         <small className="text-muted">
-          Someone famous in <cite title="Source Title">Source Title</cite>
         </small>
       </footer>
     </blockquote>
@@ -76,26 +77,29 @@ class HomePage extends Component {
       <Card.Text>
       Günlük demir dozunuzu bitki temelli beslenerek de edinebilirsiniz Sebze ağırlıklı beslenenlerin yeterince demir almadığı yaygın bir düşüncedir. Halbuki et, sebze ya da daha farklı beslenme düzeniniz de olsa demir eksikliği yaşayabilirsiniz. Demir eksikliği hem yetişkinlerde hem de çocuklarda sıklıkla görülen, çağımızın en yaygın mineral eksikliklerinden biri. Beslenme şekillerindeki farklar bile oranları değiştirmez. Beslenme düzeninize göre bolca tavuk, balık ya da yumurta yiyor olabilirsiniz; ancak bu durum demir eksikliği konusunda endişelenmemeniz gerektiği anlamına gelmiyor. Aşırı yorgunluk gibi şikayetleriniz varsa eğer, demir ölçümü yaptırmanızda fayda var.
       </Card.Text>
+    </Card.Body>
+  </Card>
+    <Card className="text-center">
+    <Card.Body>
+    <Card.Img variant="top" src="https://www.diyetkolik.com/site_media/media/customvideo_images/zayiflama_yontemleri_1.jpg" />
+      <Card.Title>Hedef kilosuna ulaşan insanların uyguladığı 6 zayıflama yöntemi</Card.Title>
       <Card.Text>
-        <small className="text-muted">Last updated 3 mins ago</small>
+      Düşük kalorili diyetler, protein diyetleri, ketojenik diyetler, şok diyetler… Sayısız diyet ve diyet listesi bulunan bu karışık dünyada, denilebilir ki herkesin diyeti kendine! Fakat hızlı zayıflama yöntemlerinin revaçta olduğu günümüzde sağlıklı yollara sapmak her şeyden önemli. Evet, önemli olan kişinin hayat tarzına, sevdiklerine ve boy, kilo, cinsiyet gibi kişisel özelliklerine uygun olarak hazırlanan bir diyet listesini takip etmesi olsa da, bazı alışkanlıklar var ki edinildiği takdirde kişinin bir bütün olarak sağlıklı bir yaşam sürdürmesini sağlıyor. Amerika'yı yeniden keşfetmek sadece vakit kaybı olur. Bunun yerine sağlıklı bir şekilde kilo vermiş kişilerin tecrübelerinden yararlanmak en mantıklısı. Bu alışkanlıklar size hızlı kilo vermeyi değil, sağlıklı beslenme ve sağlıklı yaşama yoluyla dengeli ve kalıcı bir şekilde kilo vermeyi vaat ediyor. Aşağıda sıraladığımız 6 alışkanlığı edinerek zayıflama konusunda çok önemli bir yol kat edebilir, daha da önemlisi zayıflama hedefinizi gerçekleştirdikten sonra da bulunduğunuz kiloyu koruyabilirsiniz.
+
       </Card.Text>
     </Card.Body>
   </Card>
-  <Card>
-    <Card.Img src="holder.js/100px160" />
-  </Card>
-  <Card className="text-right">
-    <blockquote className="blockquote mb-0 card-body">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>
-      <footer className="blockquote-footer">
-        <small className="text-muted">
-          Someone famous in <cite title="Source Title">Source Title</cite>
-        </small>
-      </footer>
-    </blockquote>
+
+  <Card className="text-center">
+    <Card.Body>
+    <Card.Img variant="top" src="https://www.diyetkolik.com/site_media/media/customvideo_images/paket-servisi-kapak.jpg" />
+      <Card.Title>Sağlıklı paket servis için öneriler</Card.Title>
+      <Card.Text>
+      Her ne kadar güvenli olup olmadığından emin olmadığımız için pandemi sürecinde dışarıdan sipariş vermemeye özen göstersek de bir noktada evde pişirilen yiyecek seçeneklerini tüketip çareyi paket serviste bulabiliyoruz. Ancak paket servis sırasında sağlıklı seçimler yapılması çok kolay olmuyor. Yine de sadece biraz özenli davranarak ve irademizi koruyarak bunu yapabilmek mümkün. Şimdi dikkat edilmesi gereken noktaları biraz da bizden dinleyin.
+      Bunlar arasında ; Kızarmış, çıtır çıtır, ekmekli, kremalı, soslu, peynirli, alfredo gibi kelimeler yer alıyor. Bu terimler yüksek yağ ve kalori içeriğine sahip oldukları anlamına gelebilir. Kızartılmış veya doymuş yağ oranı yüksek soslarla servis edilen ürünleri sınırlandırın. Daha sağlıklı seçenekleri oluşturanlar ızgara, haşlanmış, buharda pişirilmiş, kavrulmuş gibi kelimelerle belirtilen yağdan daha uzak ve kalorisi yükseltilmeden hazırlanmış besinlerdir.
+
+      </Card.Text>
+    </Card.Body>
   </Card>
 </CardColumns>
       </React.Fragment>
